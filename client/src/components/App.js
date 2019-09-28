104bcdf6667fa7b41782fe4c4c1f0cb2ae16bd24
Eric Lin <ttttt123465@gmail.com>
2019-09-28 23:00:46 +0800

    configure routers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import StreamList from './streams/StreamList'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from 'module./streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamShow from './streams/StreamShow'
 
 export default function App() {
     return (
         <div>
-            <h3>App</h3>
+            <BrowserRouter>
+                <div>
+                    <Route path='/' exact component={StreamList} />
+                    <Route path='/streams/new' component={StreamCreate} />
+                    <Route path='/streams/edit' component={StreamEdit} />
+                    <Route path='/streams/delete' component={StreamDelete} />
+                    <Route path='/stream/show' component={StreamShow} />
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
